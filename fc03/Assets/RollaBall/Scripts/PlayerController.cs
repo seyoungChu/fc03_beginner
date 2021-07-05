4f6dcacde79605adbdad7980e099ee9b8fd9f255
seyoung <seyoung@aceproject.co.kr>
2021-07-05 16:11:03 +0900

    [rollaball] project upload

--- fc03/Assets/RollaBall/Scripts/PlayerController.cs
+++ fc03/Assets/RollaBall/Scripts/PlayerController.cs
@@ -3,19 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 1f;
-    
+    private int Count = 0;
     private Rigidbody _rigidbody;
     private float movementX;
     private float movementY;
 
+    public TextMeshProUGUI countText;
+
+    public GameObject youWinObject;
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        Count = 0;
+        SetCountText();
+        youWinObject.SetActive(false);
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count : " + Count.ToString();
+        if (Count >= 9)
+        {
+            youWinObject.SetActive(true);
+        }
     }
 
     void OnMove(InputValue movementValue)
@@ -41,6 +56,8 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp"))
         {
             other.gameObject.SetActive(false);
+            Count = Count + 1;
+            SetCountText();
         }
         
     }
